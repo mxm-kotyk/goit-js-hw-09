b7e13a28bb49c7551df2d88963ed90f086d97bd8
Maksym Kotyk <mxm.kotyk@gmail.com>
2023-03-15 13:20:13 +0100

    First task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,59 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+let colorIsChanging = false;
+let colorChanger = 0;
+const disabledButtonStyle = 'opacity: 0.5; cursor: not-allowed;';
+
+refs.startBtn.style.cssText = styleButton(45);
+refs.stopBtn.style.cssText = `${styleButton(55)} ${disabledButtonStyle}`;
+refs.stopBtn.disabled = true;
+
+refs.startBtn.addEventListener('click', startColorChangeOnClick);
+refs.stopBtn.addEventListener('click', stopColorChangeOnClick);
+
+function startColorChangeOnClick() {
+  colorIsChanging = true;
+  colorChanger = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+    refs.startBtn.style.cssText = styleDisabledButton(45);
+    refs.stopBtn.style.cssText = colorButton(55);
+  }, 1000);
+
+  if (colorIsChanging) {
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+    refs.startBtn.style.cssText = styleDisabledButton(45);
+  }
+}
+
+function stopColorChangeOnClick() {
+  if (colorIsChanging) {
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+    refs.startBtn.style.cssText = colorButton(45);
+    refs.stopBtn.style.cssText = styleDisabledButton(55);
+  }
+  clearInterval(colorChanger);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function styleButton(left) {
+  return `position: absolute; top: 50%; left: ${left}%; transform: translate(-50%, -50%); font-weight: 700; font-size: 1.2rem; border-radius: 5px;`;
+}
+
+function colorButton(left) {
+  return `${styleButton(
+    left
+  )} background-color: ${getRandomHexColor()}; color: ${getRandomHexColor()}; border: 2px solid ${getRandomHexColor()};`;
+}
+
+function styleDisabledButton(left) {
+  return `${colorButton(left)} ${disabledButtonStyle}`;
+}
