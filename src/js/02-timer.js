2f9ffb2233345937e75e3982af23c646b3f21f13
Maksym Kotyk <mxm.kotyk@gmail.com>
2023-03-19 16:46:06 +0100

    Second task done

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,4 +1,5 @@
 import flatpickr from 'flatpickr';
+import { Report } from 'notiflix/build/notiflix-report-aio';
 import 'flatpickr/dist/flatpickr.min.css';
 import 'flatpickr/dist/themes/material_green.css';
 
@@ -20,27 +21,13 @@ refs.startBtn.disabled = true;
 refs.timerEl.style.cssText = 'display: flex; gap: 1rem';
 refs.timerFields.forEach(field => {
   field.style.cssText =
-    'display: flex; flex-direction: column; align-items: center; text-transform: uppercase';
-  field.firstElementChild.style.cssText = 'font-weight: 500; font-size: 3rem';
+    'display: flex; flex-direction: column; align-items: center; text-transform: uppercase;';
+  field.firstElementChild.style.cssText =
+    'font-weight: 500; font-size: 3rem; color: black;';
 });
 
-// const timer = {
-//   start() {
-//     const startTime = Date.now();
-
-//     setInterval(() => {
-//       const currentTime = Date.now();
-//       const deltaTIme = startTime - currentTime;
-//       const timeComponents = convertMs(deltaTIme);
-//       console.log(timeComponents);
-//     }, 1000);
-//   },
-// };
-
-// refs.startBtn.addEventListener('click', timer.start);
-
-const currentDate = new Date();
-let selectedDate;
+let selectedDate = 0;
+let timerId = 0;
 
 const flatpickrOptions = {
   enableTime: true,
@@ -49,39 +36,62 @@ const flatpickrOptions = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     selectedDate = selectedDates[0];
+    if (selectedDate <= Date.now()) {
+      Report.failure(
+        'Invalid date',
+        'Please choose a date in the future',
+        'OK'
+      );
+      return;
+    }
+
+    refs.startBtn.disabled = false;
     console.log(selectedDate);
-    pickDate(selectedDate);
   },
 };
 
 flatpickr(refs.dateInput, flatpickrOptions);
 
-function pickDate(date) {
-  if (date <= currentDate) {
-    alert('Please choose a date in the future');
+refs.startBtn.addEventListener('click', startTimerOnClick);
+
+function startTimerOnClick() {
+  timerId = setInterval(() => {
+    const delta = selectedDate - Date.now();
     refs.startBtn.disabled = true;
-    return;
-  }
-  refs.startBtn.disabled = false;
-
-  fillInTimer(date);
+    refs.dateInput.disabled = true;
+    fillInTimer(delta);
+    blinkTimer(delta);
+    stopTimerOnFinish(delta);
+  }, 1000);
 }
 
-function fillInTimer(date) {
-  const timeObj = calculateTime(date);
-
+function fillInTimer(time) {
+  const timeObj = convertMs(time);
   for (const key in clockface) {
-    clockface[key].textContent = timeObj[key];
+    clockface[key].textContent = addLeadingZero(timeObj[key]);
   }
 }
 
-// function intiateTimer() {
-//   setInterval(calculateTime, 1000);
-// }
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
 
-function calculateTime(date) {
-  const delta = date - currentDate;
-  return convertMs(delta);
+function blinkTimer(time) {
+  if (time < 10000) {
+    refs.timerFields.forEach(
+      field =>
+        (field.firstElementChild.style.color =
+          field.firstElementChild.style.color === 'black' ? 'red' : 'black')
+    );
+  }
+}
+
+function stopTimerOnFinish(time) {
+  if (time < 1000) {
+    clearInterval(timerId);
+    refs.dateInput.disabled = false;
+    Report.success(`Time's up`, '', 'OK');
+  }
 }
 
 function convertMs(ms) {
